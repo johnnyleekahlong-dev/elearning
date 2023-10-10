1271cdeeb1d47088863ab5e0511f2f039261bd61
johnnyleekahlong-dev <134179465+johnnyleekahlong-dev@users.noreply.github.com>
2023-10-10 15:22:09 +0800

    protected route

--- app/profle/page.tsx
+++ app/profle/page.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import Protected from "../hooks/useProtected";
+import Heading from "../utils/Heading";
+import Header from "../components/Header";
 
 type Props = {};
 
-const page = (props: Props) => {
-  return <div>page</div>;
+const Page = (props: Props) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [activeItem, setActiveItem] = useState<number>(0);
+  const [route, setRoute] = useState<string>("Login");
+
+  return (
+    <div>
+      <Protected>
+        <Heading
+          title="ELearning"
+          description="Elearning is a platform for students to learn"
+          keywords="Programming, MERN, Redux, Machine Learning"
+        />
+        <Header
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          setRoute={setRoute}
+          route={route}
+        />
+      </Protected>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
